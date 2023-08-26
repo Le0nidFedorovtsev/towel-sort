a7085217d7516cb748867c9aec0899eb38819fad
Leonid <103014729+Le0nidFedorovtsev@users.noreply.github.com>
2023-08-26 09:17:11 +0500

    feat: add code in index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,29 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    const mergedArr = [].concat.apply([], matrix);
+    let arrf = mergedArr.length;
+    let SuperNuzniyIndex;
+    function min(array) {    
+        let index;
+        let remember = array[0]
+        let SuperNuzniyIndex;
+        for (index = 1; index < array.length; ++index) {
+            if (array[index] < remember) {
+                remember = array[index]
+                SuperNuzniyIndex = index
+            }
+        }
+        const SuperArr = [remember, SuperNuzniyIndex]
+        return SuperArr
+    }
+    let str = []
+    let remember1;
+    let i;
+    for (i = 0; i < arrf; ++i) {
+        remember1 = min(mergedArr)
+        mergedArr.splice(remember1[1], 1);
+        str.push(remember1[0])
+    }
+    return str
 }
