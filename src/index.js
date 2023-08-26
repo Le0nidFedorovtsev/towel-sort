8edb49583dea995e6523ff67f5586a798dbc25cc
Leonid <103014729+Le0nidFedorovtsev@users.noreply.github.com>
2023-08-26 09:34:53 +0200

    fix: error with merge

--- src/index.js
+++ src/index.js
@@ -2,29 +2,72 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-    const mergedArr = [].concat.apply([], matrix);
-    let arrf = mergedArr.length;
-    let SuperNuzniyIndex;
-    function min(array) {    
-        let index;
-        let remember = array[0]
+    function min (arr) {
+        const mergedArr = arr
+        let arrf = mergedArr.length;
         let SuperNuzniyIndex;
-        for (index = 1; index < array.length; ++index) {
-            if (array[index] < remember) {
-                remember = array[index]
-                SuperNuzniyIndex = index
+        function min(array) {    
+            let index;
+            let remember = array[0]
+            let SuperNuzniyIndex;
+            for (index = 1; index < array.length; ++index) {
+                if (array[index] < remember) {
+                    remember = array[index]
+                    SuperNuzniyIndex = index
+                }
             }
+            const SuperArr = [remember, SuperNuzniyIndex]
+            return SuperArr
         }
-        const SuperArr = [remember, SuperNuzniyIndex]
-        return SuperArr
+        let str = []
+        let remember1;
+        let i;
+        for (i = 0; i < arrf; ++i) {
+            remember1 = min(mergedArr)
+            mergedArr.splice(remember1[1], 1);
+            str.push(remember1[0])
+        }
+        return str
     }
-    let str = []
-    let remember1;
-    let i;
-    for (i = 0; i < arrf; ++i) {
-        remember1 = min(mergedArr)
-        mergedArr.splice(remember1[1], 1);
-        str.push(remember1[0])
+
+    function max (arr) {
+        const mergedArr = arr
+        let arrf = mergedArr.length;
+        let SuperNuzniyIndex;
+        function min(array) {    
+            let index;
+            let remember = array[0]
+            let SuperNuzniyIndex;
+            for (index = 1; index < array.length; ++index) {
+                if (array[index] > remember) {
+                    remember = array[index]
+                    SuperNuzniyIndex = index
+                }
+            }
+            const SuperArr = [remember, SuperNuzniyIndex]
+            return SuperArr
+        }
+        let str = []
+        let remember1;
+        let i;
+        for (i = 0; i < arrf; ++i) {
+            remember1 = min(mergedArr)
+            mergedArr.splice(remember1[1], 1);
+            str.push(remember1[0])
+        }
+        return str
     }
-    return str
+    let index;
+    let result = []
+    for (index = 0; index < matrix.length; ++index) {
+        if (index === 0) {
+            result.push(min(matrix[0]))     
+        } else if (index % 2 === 0) {
+            result.push(min(matrix[index])) 
+        } else {
+            result.push(max(matrix[index])) 
+            console.log()
+        }
+    }
+    return [].concat.apply([], result);
 }
