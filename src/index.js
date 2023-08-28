67cfc0827d2d29b79fb95c915483357698f4399b
Leonid <103014729+Le0nidFedorovtsev@users.noreply.github.com>
2023-08-28 21:42:12 +0500

    fix: error with type of

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-    if (matrix === []) {
+    if (typeof matrix !== "object") {
         return []
     }
     function min (arr) {
